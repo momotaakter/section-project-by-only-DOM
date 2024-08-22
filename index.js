dd6b8fc0179d817b6fafeb41f99f5687729d51e4
momota <momotaa808@gmail.com>
2024-08-22 13:17:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+// body
+var body=document.querySelector('body');
+body.style.background='black';
+//hero-container heading create
+var hero_container=document.createElement('h3');
+body.append(hero_container);
+var h3=hero_container.innerHTML=`<h3 style='color:hsl(0,0%,100%);text-align:center;text-transform:capitalize;font-weight:normal;padding-top:70px;font-family:Trebuchet MS;
+  
+    font-weight: 900;'>wellcome to my first <span style="color:hsl(350, 98%, 64%)">DOM</span> javascript project.
+</h3>`;
+//container create
+var avobecontainer=h3;
+var container=document.querySelector('h3');
+var addcontainerr=document.createElement('div');
+container.append(addcontainerr);
+var creationDiv=addcontainerr.innerHTML=`<div style='background-color:rgba(90, 87, 87, 0.301);height:400px;margin-top:70px;margin-bottom:70px;padding:30px;display:grid;align-items:center;justify-content:center;gap:30px;    grid-template-columns:auto auto auto;'>
+<section style="background-color:black;padding:17px">
+<h4 style='color:hsl(0,0%,100%);text-align:left;text-transform:capitalize;font-weight:normal;padding-top:40px;font-family:Trebuchet MS; font-weight: 900;'> <span style="color:hsl(350, 98%, 64%)">DOM</span> is the most important for javascript.
+</h4>
+  
+<p style="color:hsl(0, 0%, 77%);text-transform:capitalize;font-family:Lucida Sans;text-align:left;font-weight:300;font-size:14px; ">orem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt eos ab ut, ea, obcaecati velit praesentium nostrum, iure totam repellat ducimus adipisci! Pariatur modi cum qui fuga ad illo tenetur exercitationem inventore sapiente quibusdam incidunt necessitatibus sed nisi, dolorum ea. Fugit, rem doloribus?</p>
+<button style="color:white;text-transform:capitalize;text-align:center;padding:12px 17px;background-color:rgba(255, 0, 0, 0.589);border:none;border-radius:30px">read more</button>
+
+</section>
+
+<section style="background-color:black;padding:17px">
+<h4 style='color:hsl(0,0%,100%);text-align:left;text-transform:capitalize;font-weight:normal;padding-top:40px;font-family:Trebuchet MS; font-weight: 900;'>it's very issential for <span style="color:hsl(350, 98%, 64%)">JAVASCRIPT</span> 
+</h4>
+  
+<p style="color:hsl(0, 0%, 77%);text-transform:capitalize;font-family:Lucida Sans;text-align:left;font-weight:300;font-size:14px; ">orem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt eos ab ut, ea, obcaecati velit praesentium nostrum, iure totam repellat ducimus adipisci! Pariatur modi cum qui fuga ad illo tenetur exercitationem inventore sapiente quibusdam incidunt necessitatibus sed nisi, dolorum ea. Fugit, rem doloribus?</p>
+
+<button style="color:white;text-transform:capitalize;text-align:center;padding:12px 17px;background-color:rgba(255, 0, 0, 0.589);border:none;border-radius:30px">read more</button>
+</section>
+
+<section style="background-color:black;padding:17px">
+<h4 style='color:hsl(0,0%,100%);text-align:left;text-transform:capitalize;font-weight:normal;padding-top:40px;font-family:Trebuchet MS; font-weight: 900;'>it's my first <span style="color:hsl(350, 98%, 64%)">PROJECT</span> of javascript. 
+</h4>
+  
+
+<p style="color:hsl(0, 0%, 77%);text-transform:capitalize;font-family:Lucida Sans;text-align:left;font-weight:300;font-size:14px; ">orem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt eos ab ut, ea, obcaecati velit praesentium nostrum, iure totam repellat ducimus adipisci! Pariatur modi cum qui fuga ad illo tenetur exercitationem inventore sapiente quibusdam incidunt necessitatibus sed nisi, dolorum ea. Fugit, rem doloribus?</p>
+
+<button style="color:white;text-transform:capitalize;text-align:center;padding:12px 17px;background-color:rgba(255, 0, 0, 0.589);border:none;border-radius:30px">read more</button>
+
+</section>
+</div>
+
+<section style='background-color:black;height:400px'>
+
+<h3 style='color:hsl(0,0%,100%);text-align:center;text-transform:capitalize;font-weight:normal;padding-top:40px;font-family:Trebuchet MS;
+    font-weight: 900;'><span style="color:hsl(350, 98%, 64%)">ABOUT</span> ME.
+</h3>
+
+<div style='display:grid;justify-content:center;gap:30px;height:400px; grid-template-columns:auto  auto;'>
+
+<img style="width:60%;"src="img.jpg" alt="">
+
+<section style="background-color:black; padding-top:20px;">
+<h4 style='color:hsl(0,0%,100%);text-align:left;text-transform:capitalize;font-weight:normal;font-family:Trebuchet MS; font-weight: 900;'>it's my first <span style="color:hsl(350, 98%, 64%)">PROJECT</span> of javascript. 
+</h4>
+  
+<p style="color:hsl(0, 0%, 77%);text-transform:capitalize;font-family:Lucida Sans;text-align:left;font-weight:300;font-size:14px; ">orem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt eos ab ut, ea, obcaecati velit praesentium nostrum, iure totam repellat ducimus adipisci! Pariatur modi cum qui fuga ad illo tenetur exercitationem inventore sapiente quibusdam incidunt necessitatibus sed nisi, dolorum ea. Fugit, rem doloribus? serunt eos ab ut, ea, obcaecati velit praesentium nostrum, iure totam repellat ducimus adipisci! Pariatur modi cum qui fuga ad illo tenetur exercitationem inventore sapiente quibusdam incidunt necessitatibus sed nisi, dolorum ea. it. Deserunt eos ab ut, ea, obcaecati velit praesentium nostrum, iure totam repellat ducimus adipisci! Pariatur modi cum qui fuga ad illo tenetur exercitationem inventore sapiente quibusdam incidunt necessitatibus sed nisi, dolorum ea. Fugit, rem doloribus? serunt eos ab ut, ea, obcaecati velit praesentium nostrum, iure totam repellat ducimus adipisci! Pariatur modi cum qui fuga ad illo tenetur exercitationem inventore sapiente qFugit, rem doloribus?</p>
+<button style="color:white;text-transform:capitalize;text-align:center;padding:12px 17px;background-color:rgba(255, 0, 0, 0.589);border:none;border-radius:30px">read more</button>
+
+</section>
+</div>
+</section>`;
+
+
